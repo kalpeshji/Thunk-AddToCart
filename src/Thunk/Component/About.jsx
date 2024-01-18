66966fb65d7a69bbe8d1c164efe647bfcc18a70e
Kalpesh <guptakalpeshji@gmail.com>
2024-01-18 23:17:57 +0530

    first commit

---
+++ src/Thunk/Component/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <h2 className='mt-6 ms-5'>About</h2>
+  )
+}
---
+++ src/Thunk/Component/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <h2 className='mt-6 ms-5'>Home</h2>
+  )
+}
